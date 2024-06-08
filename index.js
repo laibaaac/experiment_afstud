0b2febcddc77e479d1bf941f4e791cee8a833300
Laiba Choudhry <laiba@hotmail.nl>
2024-06-08 06:54:54 +0200

    close change

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-document.getElementById('close-btn').addEventListener('click', function() {
-    document.getElementById('menu-toggle').checked = false;
-  });
-  
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+const closeBtn = document.getElementById('close-btn');
+
+menuToggle.addEventListener('click', () => {
+  const isChecked = menuToggle.checked;
+  menu.classList.toggle('show');
+  if (isChecked) {
+    closeBtn.focus(); // Move focus to the close button when menu is opened
+  }
+});
